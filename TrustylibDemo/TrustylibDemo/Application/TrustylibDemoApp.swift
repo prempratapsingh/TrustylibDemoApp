8feb5e3fc70dbb94248e551feb580acc5ff8c57a
Prem Pratap Singh <prem.pratap@icloud.com>
2023-04-27 15:05:11 +0530

    Add initial xcode project source code

---
+++ TrustylibDemo/TrustylibDemo/Application/TrustylibDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrustylibDemoApp.swift
+//  TrustylibDemo
+//
+//  Created by Prem Pratap Singh on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TrustylibDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ TrustylibDemo/TrustylibDemo/View/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  TrustylibDemo
+//
+//  Created by Prem Pratap Singh on 27/04/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("TrustyLib Demo App")
+        }
+        .padding()
+    }
+}
